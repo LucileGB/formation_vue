a8d01938ef92da072e15fb4dea0e8ec45e9dc8a5
Lucile G <lucile@mail.com>
2024-03-20 14:23:35 +0100

    Added option to navigate in files

--- back/server.js
+++ back/server.js
@@ -1,1 +1,22 @@
-console.log("Hello world");
+const express = require("express");
+const serveIndex = require("serve-index");
+const app = express();
+const port = 3000;
+
+app.use((req, res, netx) => {
+  console.log("req:", req.method, req.url);
+  netx();
+});
+
+app.use(express.static("."));
+app.use(serveIndex(".", { icons: true }));
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+console.log("Ceci arrivera avant");
