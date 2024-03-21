bf986868c59f4dce5116d0c14d0f6e426c905b9c
Lucile G <lgarrigoux@hotmail.com>
2024-03-21 15:09:02 +0100

    clean up committed files

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,3 +1,6 @@
 <script setup lang="ts"></script>
 
-<template><p>Home works!</p></template>
+<template>
+  <main><h1>Gérez efficacement votre stock !</h1></main>
+  <RouterLink to="/stock" class="button">Voir le stock</RouterLink>
+</template>
