9af4732005c429eae1f609aca2f370af28d693d2
Lucile G <lucile@mail.com>
2024-03-21 12:15:48 +0100

    style header

--- front/src/layout/HeaderLayout.vue
+++ front/src/layout/HeaderLayout.vue
@@ -1,1 +1,41 @@
-<template>Header works !</template>
+<template>
+  <header>
+    <a href="#">
+      <!-- // Logo.svg : needs to add / so we know we start from the top for pathing -->
+      <img src="/logo.svg" alt="logo" />
+      <span>Gestion Stock</span>
+    </a>
+  </header>
+</template>
+
+<style scoped>
+header {
+  background: #eee;
+  height: 3em;
+  padding: 0.5em;
+  display: flex;
+  align-items: center;
+
+  a {
+    display: flex;
+    align-items: center;
+    gap: 0.5em;
+    /* IMPORTANT : inherit help harmonize*/
+    color: inherit;
+    text-decoration: inherit;
+
+    /* & hover évite d'avoir à redéclarer */
+    &:hover {
+      text-decoration: underline;
+    }
+
+    span {
+      font-weight: bold;
+      font-size: 1.3em;
+    }
+  }
+}
+img {
+  height: 2.5em;
+}
+</style>
